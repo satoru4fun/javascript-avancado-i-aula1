cc8e8582680c79492e3a3e346fc3795bb6ff20fc
Satoru Noguchi <satoru.nog@gmail.com>
2019-05-23 13:32:42 -0300

    aula 2

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,20 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {
+                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)) {
+                    return function() {
+                        console.log(`${prop} foi interceptada`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+
+                return Reflect.get(target, prop, receiver);
+            }
+        });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -19,12 +32,9 @@ class NegociacaoController {
         event.preventDefault();
                 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Negociacao adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
-
-        console.log(this._listaNegociacoes.negociacoes);
     }
 
     _criaNegociacao() {
@@ -33,6 +43,15 @@ class NegociacaoController {
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
+
+    apaga(event) {
+        event.preventDefault();
+                
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociacoes apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+        this._limpaFormulario();
     }
 
     _limpaFormulario() {
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -6,8 +6,12 @@ class ListaNegociacoes {
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
-
+    
     get negociacoes() {
         return [].concat(this._negociacoes);
+    }
+    
+    esvazia() {
+        this._negociacoes = [];
     }
 }
